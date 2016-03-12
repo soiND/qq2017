af083d202ae99b5b14ba8c00503c68a2a21c6e5c
XY <XY@163.com>
2016-03-12 10:50:39 +0800

    创建person类

---
+++ 2017qq/2017qq/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  2017qq
+//
+//  Created by xuyu on 16/3/12.
+//  Copyright © 2016年 xuyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ 2017qq/2017qq/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  2017qq
+//
+//  Created by xuyu on 16/3/12.
+//  Copyright © 2016年 xuyu. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
